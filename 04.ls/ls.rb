a3d861b21e14470b0510e3a78aadb124f5b73ece
mohira <shukatsudaison@gmail.com>
2024-09-09 13:28:12 +0900

    reactor: コメント整頓と命名変更。変数名は短さよりも自己説明的であることを優先した。

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,29 +1,25 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+MAX_NUMBER_OF_DISPLAY_COLUMNS = 3
+
 def format_entries_to_grid(entries)
   # lsでの出力形態を表現した2次元配列を作る
-
-  # 仕様: 横に「最大3列」を維持して表示する
-  # 仕様: 一般的なlsと異なり、表示端末のウィンドウ幅は表示列の数に影響しない
-  n_cols = 3
-  n_rows = (entries.count + n_cols - 1) / n_cols # 整数の切り上げ
+  number_of_display_rows = (entries.count + MAX_NUMBER_OF_DISPLAY_COLUMNS - 1) / MAX_NUMBER_OF_DISPLAY_COLUMNS
 
   # 矩形の表示枠を用意してnilは切り詰める
-  grid = Array.new(n_cols * n_rows) { |i| entries[i] }
-  grid.each_slice(n_rows)
+  grid = Array.new(MAX_NUMBER_OF_DISPLAY_COLUMNS * number_of_display_rows) { |i| entries[i] }
+  grid.each_slice(number_of_display_rows)
       .to_a.transpose
       .map { |row| row.reject(&:nil?) }
 end
 
 def stringify_entries_grid(entries_grid)
-  # 出力用の文字列を組み立てる
-
-  # 列の幅は すべて等しい かつ エントリーの最大文字長+1 で固定する
-  col_width = 1 + entries_grid.flatten.max_by(&:length).length
+  # 各表示列の幅は固定値とする
+  columns_width = 1 + entries_grid.flatten.max_by(&:length).length
 
   entries_grid.map do |row|
-    row.map { |entry| entry.ljust(col_width) }.join
+    row.map { |entry| entry.ljust(columns_width) }.join
   end.join("\n")
 end
 
